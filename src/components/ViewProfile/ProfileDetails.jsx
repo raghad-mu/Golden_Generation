33c909d811125b363e3486e4c245eda06acfd93b
Hala <halaat2003@gmail.com>
2025-06-10 12:02:29 +0300

    Display correct retiree name

--- src/components/ViewProfile/ProfileDetails.jsx
+++ src/components/ViewProfile/ProfileDetails.jsx
@@ -4,7 +4,7 @@ const ProfileDetails = ({ retireeData }) => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Profile Details</h2>
-      <p><strong>Name:</strong> {retireeData.credentials.username}</p>
+      <p><strong>Name:</strong> {retireeData.idVerification.firstName}</p>
       <p><strong>Age:</strong> {retireeData.idVerification.age}</p>
       <p><strong>Gender:</strong> {retireeData.idVerification.gender}</p>
       {/* <p><strong>Work Background:</strong> {retireeData.workBackground}</p> */}
